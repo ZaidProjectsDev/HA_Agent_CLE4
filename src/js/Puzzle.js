1f8cc62ee41bab705f9f9a0171245e267b272e92
CesianaHZ <davidj.poels@gmail.com>
2023-06-20 11:03:59 +0200

    Added playerInput and sequence testing

--- src/js/Puzzle.js
+++ src/js/Puzzle.js
@@ -1,6 +1,55 @@
 import * as ex from 'excalibur';
 
-export class Weapon extends ex.Actor
+export class Actor extends ex.Actor
 {
+    expectedSequence;
+    playerInputKb;
+    playerInputGp;
 
+    onInitialize(_engine) {
+        const expectedSequence = [];
+        const playerInputKb = [];
+        const playerInputGp = [];
+
+        this.getPlayerInput();
+
+        this.handleSequence();
+
+    }
+
+    getPlayerInput (_engine) {
+        _engine.input.keyboard.on('press', (evt) => {
+            const keypressed = evt.key;
+            this.playerInputKb.push(keypressed);
+        });
+
+        _engine.input.gamepads.on('button', (evt) => {
+            const buttonPressed = evt.button;
+            this.playerInputGp.push(buttonPressed);
+        });
+    }
+
+    handleSequence (_engine) {
+        if (this.playerInputKb.length === this.expectedSequence.length || this.playerInputGp.length === this.expectedSequence.length) {
+            let isCorrect = true;
+            for (let i = 0; i < this.expectedSequence.length; i++) {
+                if (this.playerInputKb[i] !== this.expectedSequence[i] || this.playerInputGp[i] !== this.expectedSequence[i]) {
+                    isCorrect = false;
+                    break;
+                }
+            }
+
+            if (isCorrect) {
+                // Player entered the correct sequence
+                // Do something
+            } else {
+                // Player entered the wrong sequence
+                // Do something else
+            }
+
+            // Clear player's input for the next round
+            this.playerInputKb.length = 0;
+            this.playerInputGp.length = 0;
+        }
+    }
 }
